dcc91a15d6dd250d776a0d6c01e8073abf7d35f7
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-04-19 15:13:00 +0300

    adrian-reduce

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,7 @@
+import { type } from "@testing-library/user-event/dist/type";
 import Button from "components/Button/Button";
 import Paper from "components/Paper/Paper";
-import { Component, useEffect, useState, } from "react";
+import { Component, useEffect, useState, useReducer } from "react";
 import s from "./Forms.module.css";
 
 const INITIAL_STATE = {
@@ -18,6 +19,105 @@ const TYPES = {
   CITY: "city",
   FILIAL: "filial",
 };
+
+export default Form;
+
+const reset = () => ({ type: "reset" });
+
+const formReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "firstName":
+      return { ...state, firstName: payload };
+    case "lastName":
+      return { ...state, lastName: payload };
+    case "patronymic":
+      return { ...state, patronymic: payload };
+    case "phone":
+      return { ...state, phone: payload };
+    case "email":
+      return { ...state, email: payload };
+    case "city":
+      return { ...state, city: payload };
+    case "options":
+      return { ...state, options: payload };
+    case "reset":
+      return INITIAL_STATE;
+
+    default:
+      return state;
+  }
+};
+
+function Form({ addTutor, title }) {
+  const [state, dispatch] = useReducer(formReducer, INITIAL_STATE);
+
+  const { firstName, lastName, patronymic, phone, email, city, options } =
+    state;
+
+  const onChangeInput = ({ target }) => {
+    const { value, name } = target;
+    dispatch({ type: name, payload: value });
+  };
+
+  const checkIsActive = () => {
+    return !!(
+      firstName &&
+      lastName &&
+      patronymic &&
+      phone &&
+      email &&
+      city &&
+      options
+    );
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    addTutor({
+      id: email,
+      firstName,
+      lastName,
+      patronymic,
+      phone,
+      email,
+      city,
+      options,
+    });
+
+    dispatch(reset());
+  };
+
+  return (
+    <Paper>
+      <form className={s.form} onSubmit={onSubmitForm}>
+        <h3>{title}</h3>
+
+        {TYPES.TUTORS &&
+          Object.keys(state).map((key) => (
+            <label key={key} className={s.inputLabel}>
+              <input
+                className={s.input}
+                name={key}
+                placeholder=""
+                onChange={onChangeInput}
+                value={state[key]}
+                required
+              />
+              <span className={s.text}>{key}</span>
+            </label>
+          ))}
+
+        {TYPES.CITY && <></>}
+
+        {TYPES.FILIAL && <></>}
+
+        <Button type="submit" name="ПРИГЛАСИТЬ" isActive={!checkIsActive()} />
+      </form>
+    </Paper>
+  );
+}
 
 // class Form extends Component {
 //   state = {
@@ -74,162 +174,3 @@ const TYPES = {
 //     );
 //   }
 // }
-
-export default Form;
-
-function Form({addTutor, title}) {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [patronymic, setPatronymic] = useState('');
-  const [phone, setPhone] = useState('');
-  const [email, setEmail] = useState('');
-  const [city, setCity] = useState('');
-  const [options, setOptions] = useState('');
-
-  const onChangeInput = ({ target }) => {
-    const { value, name } = target;
-
-    switch (name) {
-      case 'firstName':
-        setFirstName(value)
-        break;
-      case 'lastName':
-        setLastName(value)
-        break;
-      case 'patronymic':
-        setPatronymic(value)
-        break;
-      case 'phone':
-        setPhone(value)
-        break;
-      case 'email':
-        setEmail(value)
-        break;
-      case 'city':
-        setCity(value)
-        break;
-      case 'options':
-        setOptions(value)
-        break;
-    
-      default:
-        break;
-    }
-  };
-
-  const checkIsActive = () => {
-    return !!(firstName && lastName && patronymic && phone && email && city && options);
-  };
-
-  const resetUseState = () => {
-    setFirstName('');
-    setLastName('');
-    setPatronymic('');
-    setPhone('');
-    setEmail('');
-    setCity('');
-    setOptions('');
-  }
-
-  const onSubmitForm = (e) => {
-    e.preventDefault();
-    addTutor({ id: email,  firstName, lastName, patronymic, phone, email, city, options,});
-
-    resetUseState();
-  };
-
-  return <Paper>
-        <form className={s.form} onSubmit={onSubmitForm}>
-          <h3>{title}</h3>
-
-      {TYPES.TUTORS &&
-        (<>
-        <label className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='firstName'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={firstName}
-                  required
-                />
-                {/* <span className={s.text}>{firstName}</span> */}
-        </label>
-        <label className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='lastName'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={lastName}
-                  required
-                />
-                {/* <span className={s.text}>{lastName}</span> */}
-              </label>
-        <label  className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='patronymic'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={patronymic}
-                  required
-                />
-                {/* <span className={s.text}>{patronymic}</span> */}
-              </label>
-        <label  className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='phone'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={phone}
-                  required
-                />
-                {/* <span className={s.text}>{phone}</span> */}
-              </label>
-        <label  className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='email'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={email}
-                  required
-                />
-                {/* <span className={s.text}>{email}</span> */}
-        </label>
-        <label  className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='city'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={city}
-                  required
-                />
-                {/* <span className={s.text}>{city}</span> */}
-        </label>
-        <label className={s.inputLabel}>
-                <input
-                  className={s.input}
-                  name='options'
-                  placeholder=" "
-                  onChange={onChangeInput}
-                  value={options}
-                  required
-                />
-                {/* <span className={s.text}>{options}</span> */}
-        </label>
-      </>
-      )
-      }
-
-          {TYPES.CITY && <></>}
-
-          {TYPES.FILIAL && <></>}
-
-          <Button type="submit" name="ПРИГЛАСИТЬ" isActive={!checkIsActive()} />
-        </form>
-      </Paper>
-}
